a0bbde9a1cd18b66f3dd8cb94252a0ed21fb8340
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-09-28 20:49:16 -0700

    set up component, added bootstrap, and route for client size

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+       
+            <p>This is the Header</p>
+      
+    )
+}
+
+export default Header;
---
+++ client/src/components/SearchBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <div>
+            <p>This is the SearchBar</p>
+        </div>
+    )
+}
+
+export default SearchBar;
