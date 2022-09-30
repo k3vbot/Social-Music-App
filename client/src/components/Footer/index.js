dba5954309577eaa767655053186d2e738f166e4
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-09-29 17:18:22 -0700

    reduce card in player file

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import Card from 'react-bootstrap/Card';
-const Player = () => (
-    <Card body outline color="success" className="mx-auto my-5" style={{ width: '100rem' }}>
-        <AudioPlayer
-            autoPlay
-            src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
-            onPlay={e => console.log("onPlay")}
-        // other props here
-        />
-    </Card>
 
+const Player = () => (
+    <AudioPlayer
+        className="mx-auto my-5"
+        style={{ width: '100rem' }}
+        autoPlayAfterSrcChange
+        // autoPlay
+        src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+        onPlay={e => console.log("onPlay")}
+    // other props here
+    />
 );
 
 export default Player;
